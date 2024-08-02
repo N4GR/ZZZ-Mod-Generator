bf133b48e556ebd8eb2003cfb4cb82450f0d691d
Morgan 🏴󠁧󠁢󠁥󠁮󠁧󠁿 <107074986+N4GR@users.noreply.github.com>
2024-08-02 14:05:15 +0100

    Streamlined package imports

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import sys, os, ctypes
+import sys, ctypes
 from screeninfo import get_monitors
 
-from PyQt6 import QtWidgets, QtCore, QtGui
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QScrollArea
+from PyQt6.QtGui import QFontDatabase, QFont, QPixmap, QIcon
+from PyQt6.QtCore import QSize, Qt
 
 from config.window import windowConfig
 from config.assets import assetConfig
@@ -43,30 +44,30 @@ class MainWindow(QMainWindow):
         self.setFixedSize(window.width, window.height)
         self.setWindowTitle(window.title)
 
-        self.setWindowIcon(QtGui.QIcon(assets.images.icon))
+        self.setWindowIcon(QIcon(assets.images.icon))
 
-        label = QtWidgets.QLabel(self)
-        label.setPixmap(QtGui.QPixmap(assets.images.background))
+        label = QLabel(self)
+        label.setPixmap(QPixmap(assets.images.background))
         self.setCentralWidget(label)
         self.resize(window.width, window.height)
 
         self.setStyleSheet(r"QMainWindow {background: transparent}")
         
         self.setWindowFlags(
-            QtCore.Qt.WindowType.FramelessWindowHint
+            Qt.WindowType.FramelessWindowHint
         )
         
-        self.setAttribute(QtCore.Qt.WidgetAttribute.WA_TranslucentBackground)
+        self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
     
     def mousePressEvent(self, event):
-        if event.button() == QtCore.Qt.MouseButton.LeftButton:
+        if event.button() == Qt.MouseButton.LeftButton:
             if event.pos().y() < 100:
                 self.offset = event.pos()
         else:
             super().mousePressEvent(event)
 
     def mouseMoveEvent(self, event):
-        if self.offset is not None and event.buttons() == QtCore.Qt.MouseButton.LeftButton:
+        if self.offset is not None and event.buttons() == Qt.MouseButton.LeftButton:
             self.move(self.pos() + event.pos() - self.offset)
         else:
             super().mouseMoveEvent(event)
@@ -122,15 +123,15 @@ class images():
         self.main_window = main_window
     
     def icon(self):
-        label = QtWidgets.QLabel(self.main_window)
-        image = QtGui.QPixmap(assets.images.icon)
+        label = QLabel(self.main_window)
+        image = QPixmap(assets.images.icon)
 
         image = image.scaled(
-            QtCore.QSize(64, 64),
-            QtCore.Qt.AspectRatioMode.KeepAspectRatio,
-            QtCore.Qt.TransformationMode.SmoothTransformation
+            QSize(64, 64),
+            Qt.AspectRatioMode.KeepAspectRatio,
+            Qt.TransformationMode.SmoothTransformation
         )
-        label.resize(QtCore.QSize(64, 64))
+        label.resize(QSize(64, 64))
         label.setPixmap(image)
 
         label.move(20, 20)
@@ -139,58 +140,58 @@ class buttons():
     def __init__(self, main_window: MainWindow) -> None:
         self.main_window = main_window
         
-    def exitButton(self) -> QtWidgets.QPushButton:
+    def exitButton(self) -> QPushButton:
         def pressed():
             '''
             Changes the button icon on press.
             '''
-            button.setIcon(QtGui.QIcon(assets.buttons.exit.down))
+            button.setIcon(QIcon(assets.buttons.exit.down))
 
         def released():
             '''
             Changes the button icon on release.
             '''
-            button.setIcon(QtGui.QIcon(assets.buttons.exit.up))
+            button.setIcon(QIcon(assets.buttons.exit.up))
 
         # Creating button widget
-        button = QtWidgets.QPushButton("", self.main_window)
+        button = QPushButton("", self.main_window)
         button.clicked.connect(lambda: buttonFunctions.exitButton(button))
         button.pressed.connect(pressed)
         button.released.connect(released)
         button.setGeometry(690, 30, 80, 52)
 
         # Setting icon
-        button.setIcon(QtGui.QIcon(assets.buttons.exit.up))
-        button.setIconSize(QtCore.QSize(80, 52))
+        button.setIcon(QIcon(assets.buttons.exit.up))
+        button.setIconSize(QSize(80, 52))
 
         # Object styling handling
         button.setStyleSheet("QPushButton {background-color: transparent; border: 0px}")
 
         return button
     
-    def minimiseButton(self) -> QtWidgets.QPushButton:
+    def minimiseButton(self) -> QPushButton:
         def pressed():
             '''
             Changes the button icon on press.
             '''
-            button.setIcon(QtGui.QIcon(assets.buttons.minimise.down))
+            button.setIcon(QIcon(assets.buttons.minimise.down))
 
         def released():
             '''
             Changes the button icon on release.
             '''
-            button.setIcon(QtGui.QIcon(assets.buttons.minimise.up))
+            button.setIcon(QIcon(assets.buttons.minimise.up))
 
         # Creating button widget
-        button = QtWidgets.QPushButton("", self.main_window)
+        button = QPushButton("", self.main_window)
         button.clicked.connect(lambda: buttonFunctions.minimiseButton(button, self.main_window))
         button.pressed.connect(pressed)
         button.released.connect(released)
         button.setGeometry(600, 30, 80, 52)
 
         # Setting icon
-        button.setIcon(QtGui.QIcon(assets.buttons.minimise.up))
-        button.setIconSize(QtCore.QSize(80, 52))
+        button.setIcon(QIcon(assets.buttons.minimise.up))
+        button.setIconSize(QSize(80, 52))
 
         # Object styling handling
         button.setStyleSheet("QPushButton {background-color: transparent; border: 0px}")
@@ -198,10 +199,10 @@ class buttons():
         return button
 
 class buttonFunctions:
-    def exitButton(button: QtWidgets.QPushButton):
+    def exitButton(button: QPushButton):
         sys.exit()
     
-    def minimiseButton(button: QtWidgets.QPushButton, window: MainWindow):
+    def minimiseButton(button: QPushButton, window: MainWindow):
         window.showMinimized()
 
 

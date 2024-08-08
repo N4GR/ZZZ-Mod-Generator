2c9b6973246abd8673b361b4f573e9ad08f21277
Morgan 🏴󠁧󠁢󠁥󠁮󠁧󠁿 <107074986+N4GR@users.noreply.github.com>
2024-08-08 19:31:40 +0100

    I <3 Documentation

--- generator/scroll_area.py
+++ generator/scroll_area.py
@@ -33,6 +33,14 @@ class N4QToolButton(QToolButton):
 
 class N4QImageWithText(QWidget):
     def __init__(self, text: str, type: str, image_path: str = None, img: object = None) -> None:
+        '''QWidget object containing an image QLabel and a text QLabel positioned in a QVBoxLayout for text below images.
+        
+        Paramteres:
+            text [str]: Text to be used under the image. "Hello!"
+            type [str]: Type of item being added to the scroll area. "module"
+            image_path [str] = None: Path to the image if type = "image". "this\\path\\hello.png"
+            img [object] = None: Image.Image object if type is not "image".
+        '''
         super(N4QImageWithText, self).__init__()
         width, height = (100, 150)
 
@@ -70,9 +78,16 @@ class scrollArea():
     def __init__(self, main_window: QMainWindow, size: tuple[int] = None, max_column: int = 4) -> None:
         '''scrollArea class generator for creating scrollable areas within the main UI.
 
+        Parameters:
+            main_window [QMainWindow]: The main window of the root application.
+            size [tuple[int]] = (790, 460): Size of the scroll area (width, height). (100, 100)
+            max_column [int] = 4: Max number of columns to use when adding to scroll area. 5
+
         Attributes:
             scroll_area [QScrollArea]: The scroll area being generated.
             grid_layout [QGridLayout]: The grid layout being used inside the scroll area.
+            row [int]: The current row the scroll area is on. 6
+            column [int]: The current column the scroll area is on. 7
         '''
         self.main_window = main_window
 
@@ -108,28 +123,55 @@ class scrollArea():
         self.max_column = max_column
 
     def addItems(self, items: list[object]):
+        '''Function to add items to a scroll area.
+        
+        Parameters:
+            items [list[object]]: A list of objects that contains different things depending on item.type
+            initial [bool] = False: An indicator for uploading.py to reset column and row back to 0 if it's initiating default images.
+        '''
         def getFunc(function_name: str):
+            '''Basic function to return a function by a string.
+
+            Return:
+                function: function found from config.module.moduleFunctions from function_name string.
+            '''
             return getattr(config.module.moduleFunctions, function_name)
 
         def makeLambda(func):
+            '''Basic function to return a lambda for buttons
+            
+            Return:
+                lambda func(self.main_menu)
+            '''
             return lambda: func(self.main_window)
 
         for item in items:
+            # Column will rise up to 4 then reset to 1
+            # When column reached 4, row increases to next line.
             if self.column == self.max_column:
                 self.column = 1
                 self.row += 1
             else: self.column += 1
 
+            # Adds module type to scroll area
             if item.type == "module":
                 func = getFunc(item.function_name)
                 widget = N4QToolButton(item.name, item.thumbnail)
                 widget.clicked.connect(makeLambda(func))
                 widget.clicked.connect(self.delete)
+
+            # Adds image type to scroll area
             if item.type == "image":
                 widget = N4QImageWithText(text = f"{item.name}.{item.file_type}", type = "image", image_path = item.path)
+
+            # Adds default type to scroll area when uploading.py is called
             if item.type == "default":
                 widget = N4QImageWithText(text = f"{item.name}", type = "default", img = item.image)
 
+            # Checks if there is any item there, deletes it if it is.
+
+
+            # Adds item to the given position
             self.grid_layout.addWidget(widget, self.row, self.column)
     
     def delete(self):

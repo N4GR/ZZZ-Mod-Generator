4f8b40922c1e6dc8b77cea880932ae7136ac3221
Morgan 🏴󠁧󠁢󠁥󠁮󠁧󠁿 <107074986+N4GR@users.noreply.github.com>
2024-08-23 22:30:23 +0100

    IMPORT - QMovie, QThread

--- imports.py
+++ imports.py
@@ -1,7 +1,7 @@
 # Third party libraries
 from PyQt6.QtWidgets import QMainWindow, QPushButton, QLabel, QFileDialog, QErrorMessage, QMessageBox, QInputDialog, QApplication, QWidget, QScrollArea, QGridLayout, QToolButton, QVBoxLayout
-from PyQt6.QtGui import QPixmap, QIcon, QFontDatabase, QFont
-from PyQt6.QtCore import QSize, Qt, QPoint
+from PyQt6.QtGui import QPixmap, QIcon, QFontDatabase, QFont, QMovie
+from PyQt6.QtCore import QSize, Qt, QPoint, QThread
 
 from PIL import Image, ImageOps, ImageDraw, ImageFont, ImageEnhance, ImageChops
 from PIL.ImageQt import ImageQt
